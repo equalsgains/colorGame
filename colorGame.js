77e48a9d65527d79539e1b427a91317074192320
Luis Quintanilla <equalsgains@gmail.com>
2016-12-28 02:48:25 -0700

    color boxes will fade if you clicked on the wrong one

--- colorGame.js
+++ colorGame.js
@@ -24,7 +24,7 @@ for (var i = 0; i < squares.length; i++){
     if (clickedColor === pickedColor) {
       alert("correct");
     } else {
-      alert("wrong");
+      this.style.background = "#232323";
     }
 
   });
