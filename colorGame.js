c054a1fb9e6a3d86fa0b3be2bca0c7c802479971
Luis Quintanilla <equalsgains@gmail.com>
2016-12-28 02:37:04 -0700

    added squares and some logic on js

--- colorGame.js
+++ colorGame.js
@@ -6,7 +6,26 @@ var colors = ["rgb(255, 0, 0)",
               "rgb(255, 0, 255)",
               ];
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
 
 for (var i = 0; i < squares.length; i++){
+  //add initial colors to squares
   squares[i].style.background = colors[i];
+
+  //add click listeners to square
+  squares[i].addEventListener("click", function(){
+    //grab color of cliked square
+    var clickedColor = this.style.background;
+    //compare color to pciker
+    if (clickedColor === pickedColor) {
+      alert("correct");
+    } else {
+      alert("wrong");
+    }
+
+  });
 }
